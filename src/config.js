a0e6355aa7f7b2aafd7868d73c33e9f265b873ee
Nguyễn Văn Đức <84183427+ducduc222@users.noreply.github.com>
2023-06-07 01:07:38 +0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-    URL_API: 'https://jsonblob.com/api/1115700444713467904'
+    URL_API: 'https://jsonblob.com/api/1115700444713467904/tracks'
     // URL_API: 'http://localhost:3000/tracks'
 }
